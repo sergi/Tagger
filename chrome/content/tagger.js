f6861f1f8c73dfd8ece93fb9e078073d840fb93c
Sergi <sergi.mansilla@gmail.com>
2010-09-08 18:29:46 +0200

    Added some structure and code that will be used in the future. Currently breaking.

--- chrome/content/tagger.js
+++ chrome/content/tagger.js
@@ -1,4 +1,26 @@
 
 function ToggleTagBar() {
-    alert("tag!");
+    document.getElementById("tagger-tags").focus();
+    var tagService = Components. classes["@mozilla.org/messenger/tagservice;1"].
+                     getService (Components. interfaces. nsIMsgTagService);
+    
+    /*
+    tagService. addTag ("tag", "#33CC00", '');
+    alert(tagService.getAllTags({}))
+    ToggleMessageTag("", true)
+    */
+    
+}
+
+ToggleTagBar.prototype = {
+    getTagsForMessage: function(msg) {
+        //gDBView.hdrForFirstSelectedMessage.getStringProperty("keywords")
+        return msg.getStringProperty("keywords");
+    },
+    get inputText() {
+        return document.getElementById("tagger-tags").value;
+    },
+    set inputText(str) {
+        document.getElementById("tagger-tags").value = str;
+    }
 }
